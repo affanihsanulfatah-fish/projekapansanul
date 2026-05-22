37632c15e9c4bc6ed72422dbf4bb4b54324a3839
affanihsanulfatah-fish <affanihsanulfatah@gmail.com>
2026-05-22 14:22:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Projek Apan")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
